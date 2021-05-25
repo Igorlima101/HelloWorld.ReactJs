bfccfb9e8a900bc716ef71644029d034adc93ecd
Igor Mendonca <igorlimamendonca@gmail.com>
2021-05-24 21:16:26 -0300

    function date

--- introducao-ao-react/src/App.js
+++ introducao-ao-react/src/App.js
@@ -4,10 +4,23 @@ function Text1() {
   return "Hello World";
 }
 
+function TextDate() {
+  var today = new Date();
+  var mm = String(today.getMonth() + 1).padStart(2, "0");
+  var dd = String(today.getDate()).padStart(2, "0");
+  var yyyy = today.getFullYear();
+  var hora = today.getHours();
+  var minuto = today.getMinutes();
+  var segundo = today.getSeconds();
+
+  today =
+    dd + "/" + mm + "/" + yyyy + "    " + hora + ":" + minuto + ":" + segundo;
+  return today;
+}
 function App(){
   return (
     <div>
-    <Text1 />
+    <Text1 /> <TextDate/>
     </div>
   );
 }
